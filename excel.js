01058ed0fe18f42257fa5b8f06dc3e281078d0ef
ALEX <ALEX@MacBook-Air-toutes-ALEX.local>
2019-09-14 15:17:24 +0300

    [STEP11] Adding Js

---
+++ excel.js
@@ -0,0 +1,16 @@
+console.log("Running");
+
+let unitCol = document.querySelector("#units");
+
+console.log(unitCol);
+
+
+
+
+
+
+unitCol.style.backgroundColor= "red";
+
+unitCol.style.border="2px solid black"
+
+unitCol.textContent="595959"
