eb9ff5e6989aea59ec74be0cd48e577d397bc089
ALEX <ALEX@MacBook-Air-toutes-ALEX.local>
2019-09-14 16:54:26 +0300

    [STEP11] Function in excel

--- excel.js
+++ excel.js
@@ -9,8 +9,50 @@ console.log(unitCol);
 
 
 
-unitCol.style.backgroundColor= "red";
+// unitCol.style.backgroundColor= "red";
 
-unitCol.style.border="2px solid black"
+// unitCol.style.border="2px solid black"
 
-unitCol.textContent="595959"
+// unitCol.textContent="595959"
+
+// HTMLElement.addEventListener(EVENT:String,CALLBACK:Function);
+
+
+unitCol.addEventListener("input",handleClick);
+
+function handleClick(e){
+   //G=E*D
+
+   //E:
+    let unitsProjected= e.target;
+
+    //G:
+    let G = unitsProjected.nextElementSibling.nextElementSibling;
+    //D:
+    let D = unitsProjected.previousElementSibling;
+
+    let valE = unitsProjected.textContent;
+    let valD = D.textContent;
+
+    valE = parseFloat(valE);
+    valD = parseInt(valD);
+
+    console.log(typeof valE, typeof valD)
+
+    console.log("G = ", valE*valD);
+    
+    G.textContent= valE*valD;
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+
